c5e5e1e7778e61a8c537d3fbb26a2e9f5fda4da5
Joo Minseok <jumseok12@gmail.com>
2022-11-16 21:11:38 +0900

    하루님!!!!

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.kswkoyd.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbsparta
+client = MongoClient('mongodb+srv://teami:itogether@cluster0.2tv5dd1.mongodb.net/?retryWrites=true&w=majority')
+db = client.teampage
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route("/", methods=["GET"])
-def home():
-   tmi_list = list(db.user.find({}, {'_id': False}))
-   return jsonify({'orders': tmi_list})
-   print(tmi_list)
+def home_get():
+    tmi_lists = list(db.user.find({}, {'_id': False}))
+    return jsonify({'tmis': tmi_lists})
 
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
