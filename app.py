92b75d869d5984a24314c3571576e1d676f2df96
Joo Minseok <jumseok12@gmail.com>
2022-11-16 17:31:17 +0900

    tmi가 오길 기다리는중...

--- app.py
+++ app.py
@@ -1,28 +1,10 @@
 from pymongo import MongoClient
-import  gridfs
+client = MongoClient('mongodb+srv://test:sparta@cluster0.kswkoyd.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
 
-def mongo_conn():
-    try:
-        conn = MongoClient(host='127.0.01', port=27017)
-        print("MongoDB connection", conn)
-        return conn.grid_file
-    except Exception as e:
-        print("Error in connection", e)
+@app.route("/", methods=["GET"])
+def home():
+   tmi_list = list(db.user.find({}, {'_id': False}))
+   return jsonify({'orders': tmi_list})
+   print(tmi_list)
 
-db = mongo_conn()
-name = 'book.pdf'
-file_location = "/home/uddav/Documents/projects/" + name
-file_data = open(file_location, "rb")
-data = file_data.read()
-fs = gridfs.GridFS(db)
-fs.put(data, filename = name)
-print("upload complete")
-
-data = db.fs.files.fine_one({'filename':name})
-my_id = data['_id']
-outputdata = fs.get(my_id).read()
-download_location = "/home/uddav/Documents/projects/download/" + name
-output = open(download_location, "wb")
-output.write(outputdata)
-output.close()
-print("download complete")
