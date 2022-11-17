ed59d28e367acdebb2093b29cfbbc3a1daa6ca58
rsl150101 <dnwnd501@naver.com>
2022-11-17 15:10:36 +0900

    백엔드 시작
    URL 경로, template 지정

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
-
-from pymongo import MongoClient
-client = MongoClient('mongodb+srv://teami:itogether@cluster0.2tv5dd1.mongodb.net/?retryWrites=true&w=majority')
-db = client.teampage
-
+# from pymongo import MongoClient
+import certifi
+ca = certifi.where()
+# client = MongoClient('mongodb+srv://test:sparta@sparta.84x1qtl.mongodb.net/?retryWrites=true&w=majority',tlsCAFile=ca)
+# db = client.dbsparta
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
+@app.route('/team')
+def team():
+    return render_template('team.html')
 
-@app.route("/user", methods=["GET"])
-def user_get():
-    user_list = list(db.user.find({}, {'_id': False}))
-    return jsonify({'users': user_list})
+@app.route('/users/<username>')
+def profile(username):
+    return render_template('profile.html')
 
+@app.route('/users/<username>/photos/<photo_id>')
+def watch(username,photo_id):
+    return render_template('watch.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
