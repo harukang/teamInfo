11c9571cf3ba94a4b7866d22adbb763e5238940b
kangharu <harucometrue7@gmail.com>
2022-11-18 02:23:39 +0900

    watchpage

--- app.py
+++ app.py
@@ -44,6 +44,17 @@ def profile(user_id):
     return render_template('profile.html', user_id=user_id, avatar=avatar, name=name, mbti=mbti, style=style, blog=blog,
                            photos=photos)
 
+@app.route('/users/<user_id>/photos/<photo_id>')
+def photo(user_id,photo_id):
+    user = db.user.find_one({'num': int(user_id)})
+    photo = user['photo'][int(photo_id)-1]
+    comments = user['comment']
+    print(photo,comments,user)
+
+    return render_template('watch.html',  comments=comments, photo=photo,user_id=user_id,photo_id=photo_id)
+
+
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
