4bbc3f56ba5018439f427ce32a06ab63f780dc57
Hoseung <juwalove7@gmail.com>
2022-11-17 21:30:32 +0900

    4조 화이팅!

--- app.py
+++ app.py
@@ -5,15 +5,22 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://teami:itogether@cluster0.2tv5dd1.mongodb.net/?retryWrites=true&w=majority')
 db = client.teampage
 
+
 @app.route('/')
 def home():
    return render_template('index.html')
 
 
-@app.route("/user", methods=["GET"])
-def user_get():
-    user_list = list(db.user.find({}, {'_id': False}))
-    return jsonify({'users': user_list})
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+
+@app.route("/team/info", methods=["GET"])
+def team_get():
+    print('실행됨')
+    team_list = list(db.team.find({}, {'_id': False}))
+    return jsonify({'team': team_list})
 
 
 if __name__ == '__main__':
