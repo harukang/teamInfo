ccb0c2241a4ab46c1ad9a6b41e9d470b3523d517
rsl150101 <dnwnd501@naver.com>
2022-11-17 23:37:21 +0900

    profile 페이지 완성

--- app.py
+++ app.py
@@ -14,12 +14,6 @@ db = client.teampage
 def home():
     return render_template('index.html')
 
-
-@app.route('/team')
-def team():
-    return render_template('team.html')
-
-
 @app.route('/users/<user_id>')
 def profile(user_id):
     user = db.user.find_one({'num': int(user_id)})
@@ -30,22 +24,8 @@ def profile(user_id):
     blog = user['blog']
     photos = user['photo']
 
-    return render_template('profile.html', user_id=user_id,avatar=avatar, name=name, mbti=mbti, style=style, blog=blog,
+    return render_template('profile.html', user_id=user_id, avatar=avatar, name=name, mbti=mbti, style=style, blog=blog,
                            photos=photos)
 
-    # @app.route('/users/<user_id>', methods=['GET'])
-    # def get_profile(user_id):
-    #     user = db.user.find({},{'num':user_id})
-    #     print(user)
-
-    # @app.route('/users/<user_id>/photos/<photo_id>')
-    # def watch(user_id, photo_id):
-    #     return render_template('watch.html')
-
-    @app.route("/users/<user_id>/photos/<photo_id>")
-    def photo_get(user_id, photo_id):
-        user = db.user.find_one({'num': int(user_id)})
-        return render_template('watch.html', user_id=user_id, photo_id=photo_id)
-
-    if __name__ == '__main__':
-        app.run('0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
