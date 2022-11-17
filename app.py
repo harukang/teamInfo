017befb0c4e474b34f6ccce2b65d3d5960ff1d34
rsl150101 <dnwnd501@naver.com>
2022-11-17 21:38:01 +0900

    개인 페이지 데이터 가져오기 성공 미완성

--- app.py
+++ app.py
@@ -1,26 +1,51 @@
-from flask import Flask, render_template, request, jsonify
-app = Flask(__name__)
-# from pymongo import MongoClient
+from pymongo import MongoClient
 import certifi
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
 ca = certifi.where()
-# client = MongoClient('mongodb+srv://test:sparta@sparta.84x1qtl.mongodb.net/?retryWrites=true&w=majority',tlsCAFile=ca)
-# db = client.dbsparta
+client = MongoClient('mongodb+srv://teami:itogether@cluster0.2tv5dd1.mongodb.net/?retryWrites=true&w=majority',
+                     tlsCAFile=ca)
+db = client.teampage
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route('/team')
 def team():
     return render_template('team.html')
 
-@app.route('/users/<username>')
-def profile(username):
-    return render_template('profile.html')
 
-@app.route('/users/<username>/photos/<photo_id>')
-def watch(username,photo_id):
-    return render_template('watch.html')
+@app.route('/users/<user_id>')
+def profile(user_id):
+    user = db.user.find_one({'num': int(user_id)})
+    avatar = user['avatar']
+    name = user['name']
+    mbti = user['mbti']
+    style = user['style']
+    blog = user['blog']
+    photos = user['photo']
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    return render_template('profile.html', user_id=user_id,avatar=avatar, name=name, mbti=mbti, style=style, blog=blog,
+                           photos=photos)
+
+    # @app.route('/users/<user_id>', methods=['GET'])
+    # def get_profile(user_id):
+    #     user = db.user.find({},{'num':user_id})
+    #     print(user)
+
+    # @app.route('/users/<user_id>/photos/<photo_id>')
+    # def watch(user_id, photo_id):
+    #     return render_template('watch.html')
+
+    @app.route("/users/<user_id>/photos/<photo_id>")
+    def photo_get(user_id, photo_id):
+        user = db.user.find_one({'num': int(user_id)})
+        return render_template('watch.html', user_id=user_id, photo_id=photo_id)
+
+    if __name__ == '__main__':
+        app.run('0.0.0.0', port=5000, debug=True)
