f345780daf263fa75b9d84038ece63da1c5d0916
kangharu <harucometrue7@gmail.com>
2022-11-17 22:51:17 +0900

    app.py,index.html

--- app.py
+++ app.py
@@ -16,6 +16,14 @@ def user_get():
     user_list = list(db.user.find({}, {'_id': False}))
     return jsonify({'users': user_list})
 
+@app.route("/users/<user_id>/photos/<photo_id>")
+def photo_get(user_id, photo_id):
+        user_id = db.user.find_one({'num': int(user_id)})
+        photo = db.user.find_one({'photo'})
+        photo = user['photo']
+        comment = user['comment']
+        return render_template('watch.html', photo=photo, comment=comment)
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
